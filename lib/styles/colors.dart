84899e8238cafbfd13e0202cad4e619cdb143df5
gyuhyeong <kgh0730@gmail.com>
2024-12-10 20:27:32 +0900

    Add: Add AppColors

---
+++ lib/styles/colors.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF89A8B2);
+  static const Color secondaryColor = Color(0xFFE5E1DA);
+
+  // Blue
+  static const Color blue1 = Color(0xFFE3F4FA);
+  static const Color blue2 = Color(0xFFC8E9F5);
+  static const Color blue3 = Color(0xFFAEDFF1);
+  static const Color blue4 = Color(0xFF94D4EC);
+  static const Color blue5 = Color(0xFF89A8B2);
+  static const Color blue6 = Color(0xFF6095AC);
+  static const Color blue7 = Color(0xFF487E95);
+  static const Color blue8 = Color(0xFF30687E);
+  static const Color blue9 = Color(0xFF195168);
+  static const Color blue10 = Color(0xFF033A52);
+
+  // Ivory
+  static const Color ivory1 = Color(0xFFFDFCF9);
+  static const Color ivory2 = Color(0xFFFBF9F4);
+  static const Color ivory3 = Color(0xFFF8F5EF);
+  static const Color ivory4 = Color(0xFFF4F0E8);
+  static const Color ivory5 = Color(0xFFE5E1DA);
+  static const Color ivory6 = Color(0xFFCBC4BB);
+  static const Color ivory7 = Color(0xFFB2A89D);
+  static const Color ivory8 = Color(0xFF998D80);
+  static const Color ivory9 = Color(0xFF807469);
+  static const Color ivory10 = Color(0xFF665C54);
+
+  // Gray
+  static const Color gary1 = Color(0xFFF2F2F2);
+  static const Color gary2 = Color(0xFFE6E6E6);
+  static const Color gary3 = Color(0xFFD9D9D9);
+  static const Color gary4 = Color(0xFFCCCCCC);
+  static const Color gary5 = Color(0xFFBFBFBF);
+  static const Color gary6 = Color(0xFFB3B3B3);
+  static const Color gary7 = Color(0xFFA6A6A6);
+  static const Color gary8 = Color(0xFF999999);
+  static const Color gary9 = Color(0xFF8C8C8C);
+  static const Color gary10 = Color(0xFF808080);
+
+  // Black
+  static const Color black1 = Color(0xFF000000);
+  static const Color black2 = Color(0xFF202020);
+  static const Color white = Color(0xFFFFFFFF);
+
+  // State
+  static const Color error1 = Color(0xFFF0A8A8);
+  static const Color error2 = Color(0xFFEC9292);
+  static const Color error3 = Color(0xFFE87D7D);
+  static const Color warning = Color(0xFFF0CCA8);
+  static const Color success1 = Color(0xFFAFDEC8);
+  static const Color success2 = Color(0xFF94D1B5);
+  static const Color success3 = Color(0xFF79C3A1);
+  static const Color info = Color(0xFFA8CCF0);
+}
