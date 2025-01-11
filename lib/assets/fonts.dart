6e70b54d8e2a95fbf49db9e9fc74bd50b9a393fa
gyuhyeong <kgh0730@gmail.com>
2025-01-11 10:14:55 +0900

    feat: Add new text style to AppFonts class

---
+++ lib/assets/fonts.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppFonts {
+  static const TextStyle button1 = TextStyle(
+    fontFamily: 'Freesentation',
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    height: 1.25,
+  );
+}
