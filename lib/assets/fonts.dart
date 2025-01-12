d135d4499f0a17c175a7d1166a2d1005a5d8ad76
gyuhyeong <kgh0730@gmail.com>
2025-01-13 00:47:06 +0900

    feat: Add text style to AppFonts class

--- lib/assets/fonts.dart
+++ lib/assets/fonts.dart
@@ -1,10 +1,80 @@
 import 'package:flutter/material.dart';
 
 class AppFonts {
+  static const TextStyle h1 = TextStyle(
+    fontFamily: 'Freesentation',
+    fontSize: 22,
+    fontWeight: FontWeight.w800,
+    height: 36 / 22,
+  );
+  static const TextStyle h2 = TextStyle(
+    fontFamily: 'Freesentation',
+    fontSize: 20,
+    fontWeight: FontWeight.w800,
+    height: 32 / 20,
+  );
+
+  static const TextStyle subTitle1 = TextStyle(
+    fontFamily: 'Freesentation',
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    height: 20 / 16,
+  );
+  static const TextStyle subTitle2 = TextStyle(
+    fontFamily: 'Freesentation',
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    height: 16 / 14,
+  );
+
+  static const TextStyle body1 = TextStyle(
+    fontFamily: 'Freesentation',
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    height: 24 / 20,
+  );
+  static const TextStyle body2 = TextStyle(
+    fontFamily: 'Freesentation',
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    height: 20 / 16,
+  );
+  static const TextStyle body3 = TextStyle(
+    fontFamily: 'Freesentation',
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    height: 16 / 14,
+  );
+  static const TextStyle body4 = TextStyle(
+    fontFamily: 'Freesentation',
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    height: 16 / 14,
+  );
+
   static const TextStyle button1 = TextStyle(
     fontFamily: 'Freesentation',
     fontSize: 16,
-    fontWeight: FontWeight.bold,
-    height: 1.25,
+    fontWeight: FontWeight.w700,
+    height: 20 / 16,
+  );
+  static const TextStyle button2 = TextStyle(
+    fontFamily: 'Freesentation',
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    height: 20 / 16,
+  );
+  static const TextStyle button3 = TextStyle(
+    fontFamily: 'Freesentation',
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    height: 16 / 14,
+  );
+
+  static const TextStyle caption = TextStyle(
+    fontFamily: 'Freesentation',
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    height: 16 / 12,
   );
 }
